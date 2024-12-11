fbab039d47afdc35a5c7079d313213f3e95d8c4a
mmdincer <mmdincer@outlook.com>
2024-12-11 11:40:37 +0300

    user list ve add endpointleri tanımlandı

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const is = require('is_js');
+
+const Users = require('../db/models/Users');
+const Response = require('../lib/Response');
+const CustomError = require('../lib/Error');
+const Enum = require('../config/Enum');
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async(req, res) => {
+  try {
+    let users = await Users.find({});
+
+    res.json(Response.successResponse(users));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post('/add' , async (req, res) => {
+  let body = req.body;
+  try {
+
+    if(!body.email) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "email field must be filled");
+    if(is.not.email(body.email)) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "email field must be an email format");
+    if(!body.password) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "password field must be filled");
+    if (body.password.length < Enum.PASS_LENGTH) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "password length must be greater than " + Enum.PASS_LENGTH);
+
+    let password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(8), null);
+
+    await Users.create({
+      email: body.email,
+      password,
+      is_active: true,
+      first_name: body.first_name,
+      last_name: body.last_name,
+      phone_number: body.phone_number
+    });
+
+    res.status(Enum.HTTP_CODES.CREATED).json(Response.successResponse({success: true}, Enum.HTTP_CODES.CREATED));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
 
 module.exports = router;
