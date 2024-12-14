b8054a613923b559c3cab7b662b0137f5e959008
mmdincer <mmdincer@outlook.com>
2024-12-14 21:05:15 +0300

    user update, delete ve register endpointleri tanımlandı

--- api/routes/users.js
+++ api/routes/users.js
@@ -50,4 +50,75 @@ router.post('/add' , async (req, res) => {
   }
 });
 
+router.post('/update', async (req, res) => {
+  let body = req.body;
+  try {
+    let updates = {};
+
+    if(!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error!", "_id filed must be filled");
+
+    if(body.password && body.password.length >= Enum.PASS_LENGTH)  updates.password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(8), null);
+    if(body.first_name) updates.first_name = body.first_name;
+    if(typeof body.is_active === "boolean") updates.is_active = body.is_active;    if(body.last_name) updates.last_name = body.last_name;
+    if(body.phone_number) updates.phone_number = body.phone_number;
+
+    await Users.updateOne({_id: body._id}, updates);
+
+    res.json(Response.successResponse({success: true}));
+    
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/delete", async (req, res) => {
+  let body = req.body;
+  try {
+    if(!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error!", "_id filed must be filled");
+    
+    await Users.deleteOne({_id: body._id});
+
+    res.json(Response.successResponse({success: true}));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post('/register' , async (req, res) => {
+  let body = req.body;
+  try {
+    let user = await Users.findOne({});
+
+    if (user) {
+      return res.sendStatus(Enum.HTTP_CODES.NOT_FOUND);
+    }
+
+    if(!body.email) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "email field must be filled");
+    if(is.not.email(body.email)) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "email field must be an email format");
+    if(!body.password) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "password field must be filled");
+    if (body.password.length < Enum.PASS_LENGTH) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", "password length must be greater than " + Enum.PASS_LENGTH);
+
+
+    let password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(8), null);
+
+    await Users.create({
+      email: body.email,
+      password,
+      is_active: true,
+      first_name: body.first_name,
+      last_name: body.last_name,
+      phone_number: body.phone_number
+    });
+
+    res.status(Enum.HTTP_CODES.CREATED).json(Response.successResponse({success: true}, Enum.HTTP_CODES.CREATED));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
 module.exports = router;
